2cd95702b139b3a94d94e617bc579047ca0e7d96
João Oviedo <joaovitorgiroldi@gmail.com>
2022-05-18 16:16:07 -0300

    get all e get by id

---
+++ index.js
@@ -0,0 +1,46 @@
+const express = require('express');
+const port = 3000;
+const app = express();
+
+app.use(express.json());
+
+const produtos = [
+  {
+    id: 1,
+    sabor: 'Açaí com Leite Condensado',
+    descricao:
+      'Quam vulputate dignissim suspendisse in est ante in nibh mauris.',
+    foto: 'assets/images/acai-com-leite-condensado.png',
+    preco: 10.0,
+  },
+  {
+    id: 2,
+    sabor: 'Banana com Nutella',
+    descricao:
+      'Quam vulputate dignissim suspendisse in est ante in nibh mauris.',
+    foto: 'assets/images/banana-com-nutella.png',
+    preco: 10.0,
+  },
+  {
+    id: 3,
+    sabor: 'Chocolate Belga',
+    descricao:
+      'Quam vulputate dignissim suspendisse in est ante in nibh mauris.',
+    foto: 'assets/images/chocolate-belga.png',
+    preco: 7.0,
+  },
+];
+
+app.get('/produtos/todos-produtos', (req, res) => {
+  res.send(produtos);
+});
+
+app.get('/produtos/produto/:id', (req, res) => {
+  const idParam = Number(req.params.id);
+  const escolhaProduto = produtos.find((produto) => produto.id === idParam);
+  res.send(escolhaProduto);
+});
+
+app.listen(port, () => {
+  console.log(`Servidor rodando em http://localhost:${port}`);
+});
