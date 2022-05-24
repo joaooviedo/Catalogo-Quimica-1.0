b541c1b6d56b22b3672370972800759005df5535
João Oviedo <joaovitorgiroldi@gmail.com>
2022-05-23 22:57:22 -0300

    delete

--- assets/script.js
+++ assets/script.js
@@ -8,15 +8,23 @@ async function acheTodosProdutos() {
   produtos.forEach((produto) => {
     document.querySelector('#produtoList').insertAdjacentHTML(
       'beforeend',
-      `<div class="ProdutoListaItem">
+      `<div class="ProdutoListaItem" id="ProdutoListaItem_${produto.id}">
         <div>
             <div class="ProdutoListaItem__sabor">${produto.sabor}</div>
             <div class="ProdutoListaItem__preco">R$ ${produto.preco}</div>
             <div class="ProdutoListaItem__descricao">${produto.descricao}</div>
+            <div class="ProdutoListaItem__acoes Acoes">
+            <button class="Acoes__editar btn" onclick="abrirModal(${
+              produto.id
+            })" >Editar</button>
+            <button class="Acoes__apagar btn" onclick="abrirModalDelete(${
+              produto.id
+            })">Apagar</button>
+           </div>
           </div>
             <img class="ProdutoListaItem__foto" src=${
               produto.foto
-            } alt=${`Nome Produto ${produto.sabor}`} />
+            } alt=${`Nome do produto ${produto.sabor}`} />
         </div>`,
     );
   });
@@ -31,7 +39,7 @@ async function achePorIdProdutos() {
   const produtoEscolhidoDiv = document.querySelector('#produtoEscolhido');
 
   produtoEscolhidoDiv.innerHTML = `
-  <div class="ProdutoCardItem">
+  <div class="ProdutoCardItem" id="ProdutoListaItem_${produto.id}>
   <div>
       <div class="ProdutoCardItem__sabor">${produto.sabor}</div>
       <div class="ProdutoCardItem__preco">R$ ${produto.preco.toFixed(2)}</div>
@@ -44,21 +52,26 @@ async function achePorIdProdutos() {
 }
 acheTodosProdutos();
 
-
 async function criarProduto() {
+  const id = document.querySelector('#id').value;
   const sabor = document.querySelector('#sabor').value;
   const preco = document.querySelector('#preco').value;
   const descricao = document.querySelector('#descricao').value;
   const foto = document.querySelector('#foto').value;
 
   const produto = {
+    id,
     sabor,
     preco,
     descricao,
     foto,
   };
-  const response = await fetch(`${baseUrl}/criar-produto`, {
-    method: 'post',
+
+  const modoEdicaoAtivado = id > 0;
+  const endpoint = baseUrl + (modoEdicaoAtivado ? `/atualizar-produto/${id}` : `/criar-produto`);
+
+  const response = await fetch(endpoint, {
+    method: modoEdicaoAtivado ? 'put' : 'post',
     headers: {
       'Content-Type': 'application/json',
     },
@@ -68,34 +81,87 @@ async function criarProduto() {
 
   const novoProduto = await response.json();
 
-  const html = `<div class="ProdutoListaItem">
+  const html = `<div class="ProdutoListaItem" id="ProdutoListaItem_${produto.id}>
   <div>
     <div class="ProdutoListaItem__sabor">${novoProduto.sabor}</div>
     <div class="ProdutoListaItem__preco">R$ ${novoProduto.preco}</div>
     <div class="ProdutoListaItem__descricao">${novoProduto.descricao}</div>
+    <button class="Acoes__editar btn" onclick="abrirModal(${
+      produto.id
+    })" >Editar</button>
+    <button class="Acoes__apagar btn" onclick="abrirModalDelete(${
+      produto.id
+    })">Apagar</button>
   </div>
     <img class="ProdutoListaItem__foto" src=${
       novoProduto.foto
     } alt=${`Nome do Produto ${novoProduto.sabor}`} />
   </div>`;
 
-  document.getElementById('produtoList').insertAdjacentHTML('beforeend', html);
+  if (modoEdicaoAtivado) {
+    document.querySelector(`#ProdutoListaItem_${id}`).outerHTML = html;
+  } else {
+    document
+      .getElementById('produtoList')
+      .insertAdjacentHTML('beforeend', html);
+  }
 
-  fecharModalCadastro();
+  fecharModal();
 }
 
-async function atualizarProduto() {}
+async function abrirModal(id = null) {
+  if (id != null) {
+    document.querySelector('#title_header_modal').innerText =
+      'Atualizar um produto';
+    document.querySelector('#button-form-modal').innerText = 'Atualizar';
 
-async function deletarProduto() {}
+    const response = await fetch(`${baseUrl}/produto/${id}`);
+    const produto = await response.json();
 
-function abrirModalCadastro() {
-  document.querySelector('.modal-overlay').style.display = 'flex';
+    document.querySelector('#sabor').value = produto.sabor;
+    document.querySelector('#preco').value = produto.preco;
+    document.querySelector('#descricao').value = produto.descricao;
+    document.querySelector('#foto').value = produto.foto;
+    document.querySelector('#id').value = produto.id;
+  } else {
+    document.querySelector('#title_header_modal').innerText =
+      'Cadastrar um produto';
+    document.querySelector('#button-form-modal').innerText = 'Cadastrar';
+  }
+  document.querySelector('#overlay').style.display = 'flex';
 }
 
-function fecharModalCadastro() {
+function fecharModal() {
   document.querySelector('.modal-overlay').style.display = 'none';
   document.querySelector('#sabor').value = '';
   document.querySelector('#preco').value = 0;
   document.querySelector('#descricao').value = '';
   document.querySelector('#foto').value = '';
 }
+
+function abrirModalDelete(id) {
+  document.querySelector("#overlay-delete").style.display = "flex";
+
+  const btnSim = document.querySelector(".btn_delete_yes")
+
+  btnSim.addEventListener("click", function() {
+    deletarProduto(id);
+  })
+}
+function fecharModalDelete() {
+  document.querySelector("#overlay-delete").style.display = "none";
+}
+
+const deletarProduto = async (id) => {
+  const response = await fetch(`${baseUrl}/deletar-produto/${id}`, {
+    method: "delete",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    mode: "cors",
+  });
+  const result = await response.json();
+  alert(result.message)
+  document.getElementById("produtoList").innerHTML = ""
+  acheTodosProdutos()
+};
