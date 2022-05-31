11b10de8aedf9634cd2986d91cdc7174a7006156
João Oviedo <joaovitorgiroldi@gmail.com>
2022-05-31 17:40:10 -0300

    mongoose

--- index.js
+++ index.js
@@ -1,9 +1,12 @@
 const express = require('express');
 const cors = require('cors');
 const route = require('./src/routes/produto.route');
+const connectToDatabase = require('./src/database/database');
 
 const port = 3000;
 const app = express();
+
+connectToDatabase();
 
 app.use(express.json());
 app.use(cors());
